66e4a4bd0da25973f0489026568e2a2f15466e4a
tylersiskar <36086162+tylersiskar@users.noreply.github.com>
2026-03-18 10:39:31 -0400

    port

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ import {
 const app = express();
 app.use(cors());
 app.use(express.json({ limit: "10mb" }));
-const port = 5001;
+const port = 5000;
 
 app.use("/api", routes);
 
@@ -45,6 +45,7 @@ app.get("/syncLeague", async (req, res) => {
   try {
     await syncLeague(process.env.LEAGUE_ID);
     res.send("League synced successfully!");
+    1;
   } catch (err) {
     res.status(500).send(err.message);
   }
